66a2a480c4d692b6893ce1c34340647e748dd57e
Alexander Khotemskoy <Xotabu4@gmail.com>
2016-11-29 18:53:41 +0200

    Added promise example
    Added promise example

--- spec.js
+++ spec.js
@@ -79,4 +79,16 @@ describe('Preserver tests', function () {
         expect(notesPage.notes.count()).toBe(0, 'Notes count should be 0')
     })
 
+    xit('promise example - how to handle promise rejections', function () {
+
+        //Searching for element that does not exist
+        let nonExists = $('SOMENONEXTISTING ELEMENT!')
+
+        // Notice that in this case error will not be thrown, and will not break the test
+        nonExists.getText().then(
+            text=> console.log(text),
+            err=> console.log('CANNOT FIND YOUR ELEMENT')
+        )
+    })
 })
+
